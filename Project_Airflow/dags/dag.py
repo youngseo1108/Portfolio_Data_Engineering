9a4741b7cc90d666f2deb0bfac0f61e444702d4b
Youngseo <0youngseo1108@gmail.com>
2025-08-07 13:31:18 +0900

    fixing the cursor issue - pandas, sqlalchemy version incompatibility, forced sqlalchemy in .yaml

--- Project_Airflow/dags/dag.py
+++ Project_Airflow/dags/dag.py
@@ -1,22 +1,26 @@
-# from airflow import DAG
-from airflow.decorators import dag
-from airflow.operators.python import PythonOperator
+# from airflow.sdk import DAG
+from airflow.sdk import dag
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
 from datetime import datetime, timedelta
 import pandas as pd
+
 
 def log_failure(context):
   dag_run = context.get("dag_run")
   task = context.get("task_instance")
   print(f"Warning: Task {task.task_id} failed during run {dag_run}")
 
+# Setting default arguments
 default_args = {
   'owner': 'airflow',
   'retries': 1,
   'retry_delay': timedelta(minutes=5)
 }
 
+
+# Define the DAG
 @dag(
   dag_id='merchant_etl_dag',
   default_args=default_args,
@@ -25,21 +29,35 @@ default_args = {
   catchup=False
 )
 
+
 def merchant_etl_workflow():
 
   start = EmptyOperator(task_id='start')
   end = EmptyOperator(task_id='end')
 
   def load_data_to_postgres():
+    '''
+    Extract and load csv to postgres
+    '''
     file_path = '/opt/airflow/dags/data/merchant_transactions.csv'
     df = pd.read_csv(file_path)
         
     pg_hook = PostgresHook(postgres_conn_id='transactions_connection')
     engine = pg_hook.get_sqlalchemy_engine()
-    
-    df.to_sql('raw_transactions', con=engine, if_exists='append', index=False, method='multi')
+
+    with engine.connect() as connection:
+      df.to_sql(
+        name='raw_transactions',
+        con=connection,
+        if_exists='append',
+        index=False,
+        method='multi'
+      )
 
   def create_merchant_profiles():
+    '''
+    Transform the data
+    '''
     pg_hook = PostgresHook(postgres_conn_id='transactions_connection')
     conn = pg_hook.get_conn()
     cursor = conn.cursor()
@@ -63,7 +81,8 @@ def merchant_etl_workflow():
 
   task_load_csv = PythonOperator(
     task_id='load_csv_to_postgres',
-    python_callable=load_data_to_postgres
+    python_callable=load_data_to_postgres,
+    on_failure_callback=log_failure
   )
 
   def validate_raw_data():
@@ -96,6 +115,8 @@ def merchant_etl_workflow():
     python_callable=create_merchant_profiles
   )
 
+  # Chain operations
   start >> task_load_csv >> task_validate >> task_create_profiles >> end
+
 
 dag = merchant_etl_workflow()
